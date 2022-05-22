8826b68af93957e7bac24b38e431d5bb07cb5049
Puyodead1 <14828766+Puyodead1@users.noreply.github.com>
2022-05-22 13:22:42 -0400

    add example instance config

---
+++ instance/config.example.py
@@ -0,0 +1,4 @@
+# generate secret with https://generate-secret.vercel.app/32
+SECRET_KEY = ""
+OAUTH2_CLIENT_ID = ""  # Discord OAuth Client ID
+OAUTH2_CLIENT_SECRET = ""  # Discord OAuth Client Secret
