13c725841eacdc50a6937c6686037139e5aca754
Notaghost9997 <65109659+Notaghost9997@users.noreply.github.com>
2022-05-31 03:31:41 +0500

    changed port to 8080 from 5001

--- app.py
+++ app.py
@@ -296,4 +296,4 @@ def pssh():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
