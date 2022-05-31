2441080a578544cbc240324f12393243adc75018
Puyodead1 <14828766+Puyodead1@users.noreply.github.com>
2022-05-30 21:04:28 -0400

    cdms in profile + bug fixes
    - fixed attribute error when adding keys
    - session is cleared if login error
    - user id is associated with uploaded cdm
    - profile page now shows all the users uploaded cdms
    - Updated layout of profile page

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 DEBUG = True  # Flask debugging, is auto loaded by flask
 PROXY = {}
 DEFAULT_CDMS = [
@@ -13,4 +15,5 @@ APPENDERS = ["staff_getwvkeys", "seopsta0197123"]
 GUILD_ID = "948675767754174465"
 VERIFIED_ROLE_ID = "970332150891155607"
 LOGIN_DISABLED = False
-OAUTH2_REDIRECT_URL = "http://getwvkeys.cc/login/callback"
+OAUTH2_REDIRECT_URL = "http://localhost:8080/login/callback" if os.environ[
+    "DEVELOPMENT"] else "http://getwvkeys.cc/login/callback"
