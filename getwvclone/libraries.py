3ebd783422f64fe7d238f91b2bbcac983d4465d1
Puyodead1 <23562356riley@gmail.com>
2022-06-25 21:41:04 -0400

    add support for server certificates

--- getwvclone/libraries.py
+++ getwvclone/libraries.py
@@ -27,6 +27,20 @@ from getwvclone.utils import (
 
 logger = logging.getLogger("getwvkeys")
 
+common_privacy_cert = (
+    "CAUSxwUKwQIIAxIQFwW5F8wSBIaLBjM6L3cqjBiCtIKSBSKOAjCCAQoCggEBAJntWzsyfateJO/DtiqVtZhSCtW8y"
+    "zdQPgZFuBTYdrjfQFEEQa2M462xG7iMTnJaXkqeB5UpHVhYQCOn4a8OOKkSeTkwCGELbxWMh4x+Ib/7/up34QGeHl"
+    "eB6KRfRiY9FOYOgFioYHrc4E+shFexN6jWfM3rM3BdmDoh+07svUoQykdJDKR+ql1DghjduvHK3jOS8T1v+2RC/TH"
+    "hv0CwxgTRxLpMlSCkv5fuvWCSmvzu9Vu69WTi0Ods18Vcc6CCuZYSC4NZ7c4kcHCCaA1vZ8bYLErF8xNEkKdO7Dev"
+    "Sy8BDFnoKEPiWC8La59dsPxebt9k+9MItHEbzxJQAZyfWgkCAwEAAToUbGljZW5zZS53aWRldmluZS5jb20SgAOuN"
+    "HMUtag1KX8nE4j7e7jLUnfSSYI83dHaMLkzOVEes8y96gS5RLknwSE0bv296snUE5F+bsF2oQQ4RgpQO8GVK5uk5M"
+    "4PxL/CCpgIqq9L/NGcHc/N9XTMrCjRtBBBbPneiAQwHL2zNMr80NQJeEI6ZC5UYT3wr8+WykqSSdhV5Cs6cD7xdn9"
+    "qm9Nta/gr52u/DLpP3lnSq8x2/rZCR7hcQx+8pSJmthn8NpeVQ/ypy727+voOGlXnVaPHvOZV+WRvWCq5z3CqCLl5"
+    "+Gf2Ogsrf9s2LFvE7NVV2FvKqcWTw4PIV9Sdqrd+QLeFHd/SSZiAjjWyWOddeOrAyhb3BHMEwg2T7eTo/xxvF+YkP"
+    "j89qPwXCYcOxF+6gjomPwzvofcJOxkJkoMmMzcFBDopvab5tDQsyN9UPLGhGC98X/8z8QSQ+spbJTYLdgFenFoGq4"
+    "7gLwDS6NWYYQSqzE3Udf2W7pzk4ybyG4PHBYV3s4cyzdq8amvtE/sNSdOKReuHpfQ="
+)
+
 
 class Library:
     def __init__(self, db: SQLAlchemy):
@@ -118,8 +132,9 @@ class Pywidevine:
         cache=False,
         response=None,
         challenge=False,
-        server_certificate=None,
+        server_certificate=common_privacy_cert,
         session_id=None,
+        disable_privacy=False,
     ):
         self.library = library
         self.license_url = license_url
@@ -133,7 +148,7 @@ class Pywidevine:
         self.challenge = challenge
         self.response = response
         self.user_id = user_id
-        self.server_certificate = server_certificate
+        self.server_certificate = None if disable_privacy else server_certificate
         self.proxy = proxy
         self.store_request = {}
         self.session_id = session_id
