1a2586fea26436d82257154485b65c356946cb8f
Puyodead1 <puyodead@proton.me>
2024-12-03 17:55:29 -0500

    I hate migrations

--- alembic/versions/51d65f145c1f_create_prd.py
+++ alembic/versions/51d65f145c1f_create_prd.py
@@ -26,30 +26,20 @@ def upgrade() -> None:
         sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
         sa.Column("hash", sa.String(length=255), nullable=False),
         sa.Column("prd", sa.Text(), nullable=False),
-        sa.Column(
-            "uploaded_by",
-            sa.String(length=19, collation="utf8mb4_general_ci"),
-            nullable=False,
-        ),
-        sa.PrimaryKeyConstraint("id"),
-        sa.UniqueConstraint("hash"),
+        sa.Column("uploaded_by", sa.String(length=19), nullable=False),
         sa.ForeignKeyConstraint(
             ["uploaded_by"],
             ["users.id"],
         ),
-        mysql_engine="InnoDB",
-        mysql_charset="utf8mb4",
-        mysql_collate="utf8mb4_general_ci",
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("hash"),
     )
     op.create_table(
         "user_prd",
-        sa.Column("user_id", sa.String(length=19, collation="utf8mb4_general_ci"), nullable=False),
-        sa.Column("device_hash", sa.String(length=255, collation="utf8mb4_general_ci"), nullable=False),
+        sa.Column("user_id", sa.String(length=19), nullable=False),
+        sa.Column("device_hash", sa.String(length=255), nullable=False),
         sa.ForeignKeyConstraint(["device_hash"], ["prds.hash"], ondelete="CASCADE"),
         sa.ForeignKeyConstraint(["user_id"], ["users.id"], ondelete="CASCADE"),
-        mysql_engine="InnoDB",
-        mysql_charset="utf8mb4",
-        mysql_collate="utf8mb4_general_ci",
     )
     # ### end Alembic commands ###
 
