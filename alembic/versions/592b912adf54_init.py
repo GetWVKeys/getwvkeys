5f590040e6adbdd10de9de3b028d3b93e37f975e
GitHub Action <action@github.com>
2024-12-03 21:53:27 +0000

    isort

--- alembic/versions/592b912adf54_init.py
+++ alembic/versions/592b912adf54_init.py
@@ -7,9 +7,9 @@ Create Date: 2024-12-02 13:22:59.911782
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '592b912adf54'
